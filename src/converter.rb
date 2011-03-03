c2dda1987553880f5c79e3598c61c296b54490a0
Guy Royse <guy@guyroyse.com>
2011-03-02 19:52:39 -0500

    Now handles command line args

--- src/converter.rb
+++ src/converter.rb
@@ -36,7 +36,3 @@ def parse_package(full_name)
   temp = /^.*\./.match full_name
   return /^.*[^\.]/.match(temp.to_s)
 end
-
-input = STDIN.read
-output = convert input, 'jar', 'ncloc', 'coverage'
-puts output 
